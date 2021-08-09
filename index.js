e803923fd126f6bb7026f497a7200bb467f200cb
pavanleton <pavankumarkpkp@gmail.com>
2021-08-09 20:46:02 +0530

    add inital load

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const taskContainer = document.querySelector(".task__container");
+let globalTaskData = [];
 
 const addNewCard = () => {
   // get task data
@@ -9,6 +10,11 @@ const addNewCard = () => {
     type: document.getElementById("taskType").value,
     description: document.getElementById("taskDescription").value,
   };
+
+  globalTaskData.push(taskData);
+
+  // update the localstorage
+  localStorage.setItem("taskyCA", JSON.stringify({ card: globalTaskData }));
 
   // generate HTML code
 
@@ -51,3 +57,57 @@ const addNewCard = () => {
 
   return;
 };
+
+const loadExistingCards = () => {
+  // check localstorage
+  const getData = localStorage.getItem("taskyCA");
+
+  // parse JSON data, if exist
+  if (!getData) return;
+
+  const taskCards = JSON.parse(getData);
+
+  globalTaskData = taskCards.card;
+
+  globalTaskData.map((taskData) => {
+    // generate HTML code for those data
+    const newCard = ` <div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
+  <div class="card">
+    <div class="card-header gap-2 d-flex justify-content-end">
+      <button class="btn btn-outline-info">
+        <i class="fal fa-pencil"></i>
+      </button>
+      <button class="btn btn-outline-danger">
+        <i class="far fa-trash-alt"></i>
+      </button>
+    </div>
+    <div class="card-body">
+      <img
+        src=${taskData.image}
+        alt="image"
+        class="card-img"
+      />
+      <h5 class="card-title mt-4">${taskData.title}</h5>
+      <p class="card-text">
+        ${taskData.description}
+      </p>
+      <span class="badge bg-primary">${taskData.type}</span>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-outline-primary">Open Task</button>
+    </div>
+  </div>
+</div>`;
+
+    // inject to the DOM
+    taskContainer.insertAdjacentHTML("beforeend", newCard);
+  });
+
+  return;
+};
+
+// Strigify
+// JS object -> JSON
+
+// Parse
+// JSON -> JS objects
